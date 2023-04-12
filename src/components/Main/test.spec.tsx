d4061cac1f85f3c6e93951f79b202241323e0efe
felixstenian <felixstenian@gmail.com>
2023-04-12 15:47:41 -0300

    fix: updates in tests

--- src/components/Main/test.spec.tsx
+++ src/components/Main/test.spec.tsx
@@ -1,23 +1,23 @@
-import { render, screen } from '@testing-library/react'
+import { screen } from '@testing-library/react'
 
+import { renderWithTheme } from 'utils'
 import Main from '.'
 
 describe('<Main />', () => {
   it('1. should render the reading', () => {
-    const { container } = render(<Main />)
+    const { container } = renderWithTheme(<Main />)
 
-    expect(
-      screen.getByRole('heading', { name: /react avançado/i })
-    ).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: /buscar/i })).toBeInTheDocument()
+    expect(screen.getByText(/LOGO/i)).toBeInTheDocument()
 
     expect(container.firstChild).toMatchSnapshot()
   })
 
   it('should render the color correctly', () => {
-    const { container } = render(<Main />)
+    const { container } = renderWithTheme(<Main />)
 
     expect(container.firstChild).toHaveStyle({
-      'background-color': '#06092b'
+      'background-color': '#FAFAFA'
     })
   })
 })
