db396283b2922f8b1b7d76d4745fdc44c4983acb
felixstenian <felixstenian@gmail.com>
2023-04-11 22:16:28 -0300

    fix: update baseUrl and imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Main } from '@/components'
+import { Main } from 'components'
 
 export default function Home() {
   return <Main />
--- src/utils/tests/renderWithTheme.tsx
+++ src/utils/tests/renderWithTheme.tsx
@@ -1,7 +1,7 @@
 import { RenderResult, render } from '@testing-library/react'
 import { ThemeProvider } from 'styled-components'
 
-import { theme } from '@/styles'
+import { theme } from 'styles'
 
 export const renderWithTheme = (children: React.ReactNode): RenderResult =>
   render(<ThemeProvider theme={theme}>{children}</ThemeProvider>)
