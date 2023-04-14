ed67f7264671c79ee0ac2eb69457682285d13920
felixstenian <felixstenian@gmail.com>
2023-04-14 15:40:26 -0300

    refactor: update import interface and styles

--- src/components/UsersList/UsersList.tsx
+++ src/components/UsersList/UsersList.tsx
@@ -1,13 +1,20 @@
 import { Row } from 'components'
 
-import { CardUser, User } from '.'
+import { User } from 'shared'
+
+import { CardUser } from '.'
 
 export type UsersListProps = {
   listUsers: User[]
 }
 
 const UsersList = ({ listUsers }: UsersListProps) => (
-  <Row flexDirection="column" px={10} data-testid="listUsers">
+  <Row
+    flexDirection="column"
+    px={10}
+    data-testid="listUsers"
+    alignItems="center"
+  >
     {listUsers?.map((user) => (
       <CardUser key={user.id.value} userData={user} />
     ))}
--- src/components/UsersList/index.ts
+++ src/components/UsersList/index.ts
@@ -1,4 +1,4 @@
 export { default as CardUser } from './CardUser'
-export type { CardUserProps, User } from './CardUser'
+export type { CardUserProps } from './CardUser'
 export { default } from './UsersList'
 export type { UsersListProps } from './UsersList'
