bdb320e3a76d10fad6b812757f0fe41c0fbcd641
felixstenian <felixstenian@gmail.com>
2023-04-17 12:05:58 -0300

    refactor: replace and add constants

--- src/shared/helpers/constants.ts
+++ src/shared/helpers/constants.ts
@@ -1,23 +1,105 @@
-export const NATIONALITIES = {
-  AU: 'Austrália (Australia)',
-  BR: 'Brasil',
-  CA: 'Canadá',
-  CH: 'Suíça (Switzerland)',
-  DE: 'Alemanha (Germany)',
-  DK: 'Dinamarca (Denmark)',
-  ES: 'Espanha (Spain)',
-  FI: 'Finlândia (Finland)',
-  FR: 'França (France)',
-  GB: 'Reino Unido (United Kingdom)',
-  IE: 'Irlanda (Ireland)',
-  IN: 'Índia',
-  IR: 'Irã',
-  MX: 'México',
-  NL: 'Países Baixos (Netherlands)',
-  NO: 'Noruega (Norway)',
-  NZ: 'Nova Zelândia (New Zealand)',
-  RS: 'Sérvia (Serbia)',
-  TR: 'Turquia (Türkiye)',
-  UA: 'Ucrânia (Ukraine)',
-  US: 'Estados Unidos (United States)'
-}
+export const GENDERS = [
+  {
+    label: 'Todos',
+    value: ''
+  },
+  {
+    label: 'Feminino',
+    value: 'female'
+  },
+  {
+    label: 'Masculino',
+    value: 'male'
+  }
+]
+
+export const NATIONALITIES = [
+  {
+    label: 'Todos',
+    value: ''
+  },
+  {
+    label: 'Austrália (Australia)',
+    value: 'AU'
+  },
+  {
+    label: 'Brasil',
+    value: 'BR'
+  },
+  {
+    label: 'Canadá',
+    value: 'CA'
+  },
+  {
+    label: 'Suíça (Switzerland)',
+    value: 'CH'
+  },
+  {
+    label: 'Alemanha (Germany)',
+    value: 'DE'
+  },
+  {
+    label: 'Dinamarca (Denmark)',
+    value: 'DK'
+  },
+  {
+    label: 'Espanha (Spain)',
+    value: 'ES'
+  },
+  {
+    label: 'Finlândia (Finland)',
+    value: 'FI'
+  },
+  {
+    label: 'França (France)',
+    value: 'FR'
+  },
+  {
+    label: 'Reino Unido (United Kingdom)',
+    value: 'GB'
+  },
+  {
+    label: 'Irlanda (Ireland)',
+    value: 'IE'
+  },
+  {
+    label: 'Índia',
+    value: 'IN'
+  },
+  {
+    label: 'Irã',
+    value: 'IR'
+  },
+  {
+    label: 'México',
+    value: 'MX'
+  },
+  {
+    label: 'Países Baixos (Netherlands)',
+    value: 'NL'
+  },
+  {
+    label: 'Noruega (Norway)',
+    value: 'NO'
+  },
+  {
+    label: 'Nova Zelândia (New Zealand)',
+    value: 'NZ'
+  },
+  {
+    label: 'Sérvia (Serbia)',
+    value: 'RS'
+  },
+  {
+    label: 'Turquia (Türkiye)',
+    value: 'TR'
+  },
+  {
+    label: 'Ucrânia (Ukraine)',
+    value: 'UA'
+  },
+  {
+    label: 'Estados Unidos (United States)',
+    value: 'US'
+  }
+]
