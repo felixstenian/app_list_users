a6175954442e9f946c3c139873b3bb0bcc330061
felixstenian <felixstenian@gmail.com>
2023-04-17 12:10:54 -0300

    refactor: removing type and add modal user details

--- src/components/UsersList/CardUser.tsx
+++ src/components/UsersList/CardUser.tsx
@@ -1,93 +1,68 @@
-import { Image, Row, Text } from 'components'
-
-export interface User {
-  gender: string
-  name: {
-    title: string
-    first: string
-    last: string
-  }
-  location: {
-    street: {
-      number: number
-      name: string
-    }
-    city: string
-    state: string
-    country: string
-    postcode: string | number
-    coordinates: {
-      latitude: string
-      longitude: string
-    }
-    timezone: {
-      offset: string
-      description: string
-    }
-  }
-  email: string
-  login: {
-    uuid: string
-    username: string
-    password: string
-    salt: string
-    md5: string
-    sha1: string
-    sha256: string
-  }
-  dob: {
-    date: string
-    age: number
-  }
-  registered: {
-    date: string
-    age: number
-  }
-  phone: string
-  cell: string
-  id: {
-    name: string
-    value: string | null
-  }
-  picture: {
-    large: string
-    medium: string
-    thumbnail: string
-  }
-  nat: string
-}
+import { Image, ModalUserDetails, Row, Text } from 'components'
+import { useState } from 'react'
+import { User } from 'shared/types'
 
 export type CardUserProps = {
   userData: User
 }
 
 const CardUser = ({ userData }: CardUserProps) => {
+  const [openProfile, setOpenProfile] = useState<boolean>(false)
+
+  const displayDataUser = () => setOpenProfile(true)
+  const onCloseUserDetails = () => setOpenProfile(false)
+
   return (
-    <Row
-      key={userData.login.uuid}
-      bg="secondary"
-      width="100%"
-      justifyContent="space-between"
-      alignItems="center"
-      p={10}
-      borderRadius="8px"
-      my="5px"
-      flexDirection={['column', 'column', 'row']}
-      gap="1rem"
-    >
-      <Row alignItems="center" flexDirection={['column', 'column', 'row']}>
-        <Image
-          src={userData?.picture?.large}
-          alt="alt"
-          width={['10rem', '12rem', '8rem']}
-          height={['10rem', '12rem', '8rem']}
-          borderRadius="10rem"
+    <>
+      {openProfile && (
+        <ModalUserDetails
+          userData={userData}
+          open={openProfile}
+          onClose={onCloseUserDetails}
         />
-        <Text ml={[0, 0, 10]}>{userData?.name?.first}</Text>
+      )}
+      <Row
+        key={userData.login.uuid}
+        bg="secondary"
+        width="80%"
+        justifyContent="center"
+        alignItems="center"
+        p={10}
+        borderRadius="8px"
+        my="5px"
+        gap="1rem"
+        flexWrap="wrap"
+        minWidth="32rem"
+        as="a"
+        onClick={displayDataUser}
+        cursor="pointer"
+      >
+        <Row
+          alignItems="center"
+          justifyContent={['center', 'flex-start', 'flex-start']}
+          flex={1}
+        >
+          <Image
+            src={userData?.picture?.large}
+            alt="alt"
+            width={['10rem', '12rem', '8rem']}
+            height={['10rem', '12rem', '8rem']}
+            borderRadius="10rem"
+          />
+          <Text ml={[30, 20, 20]} variant="mediumTitle">
+            {userData?.name?.first}
+          </Text>
+        </Row>
+        <Row
+          flexWrap="wrap"
+          justifyContent="center"
+          gap={['4rem', '3rem', '5rem']}
+        >
+          <Text variant="subtitle">{userData?.location?.country}</Text>
+          <Text>{userData?.email}</Text>
+        </Row>
       </Row>
-      <div>{userData?.location?.country}</div>
-      <div>{userData?.email}</div>
-    </Row>
+    </>
   )
 }
 
